cf83faa04df90fead5dfc9de1311179835b78611
Wehde <kilianwehde@outlook.com>
2022-08-25 20:14:23 -0500

    Delete WebAppHelloWorld/WebAppHelloWorld directory

--- WebAppHelloWorld/WebAppHelloWorld/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppHelloWorld.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
