b8200c737f18abfd371e9419793af4158194683b
Wehde <kewehde@dmacc.edy>
2022-08-27 23:34:26 -0500

    Finished Assignment

---
+++ FirstResponsiveWebAppWehde/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using FirstResponsiveWebAppWehde.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppWehde.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(GetAgeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = model.CaclulateAge();
+            }
+            else
+            {
+                ViewBag.Age = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppWehde/Models/GetAgeModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppWehde.Models
+{
+    public class GetAgeModel
+    {
+        public DateTime TODAY = DateTime.Now;
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string? UsersName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your birthday")]
+        public DateTime? UserBirthday { get; set; }
+
+        public string? CaclulateAge()
+        {
+            int age = 0;
+            if(UserBirthday != null)
+            {
+                age += TODAY.Year - UserBirthday.Value.Year;
+                if (TODAY < UserBirthday.Value.AddYears(age))
+                {
+                    age--;
+                }
+            }
+            return UsersName + " your age is " + age + " as of today.";
+        }
+    }
+}
